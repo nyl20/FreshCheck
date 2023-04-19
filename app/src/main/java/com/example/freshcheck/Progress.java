94a93ace11ea82615816944ed749f6ddc2a91016
nyl20 <nicole.yliao@gmail.com>
2023-04-19 15:27:45 -0400

    progress button added

---
+++ app/src/main/java/com/example/freshcheck/Progress.java
@@ -0,0 +1,23 @@
+package com.example.freshcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Progress extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.visuals);
+    }
+}
+
+
+//import android.support.v7.app.AppCompatActivity;
+//        import android.os.Bundle;
+//public class NewActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_new);
+//    }
+//}

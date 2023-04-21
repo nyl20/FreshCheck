8d0f83b42f3d1a781d379ce6c8ed6e5de9662d47
nyl20 <nicole.yliao@gmail.com>
2023-04-21 04:35:19 -0400

    chart library and first bar chart

--- app/src/main/java/com/example/freshcheck/Progress.java
+++ app/src/main/java/com/example/freshcheck/Progress.java
@@ -1,13 +1,51 @@
 package com.example.freshcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class Progress extends AppCompatActivity {
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+
+    ArrayList barEntriesArrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visuals);
+
+        barChart = findViewById(R.id.chart1);
+        getBarEntries();
+        barDataSet = new BarDataSet(barEntriesArrayList, "progress");
+        barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
+    }
+
+    private void getBarEntries(){
+        barEntriesArrayList = new ArrayList<>();
+        barEntriesArrayList.add(new BarEntry(1f, 4));
+        barEntriesArrayList.add(new BarEntry(2f, 6));
+        barEntriesArrayList.add(new BarEntry(3f, 8));
+        barEntriesArrayList.add(new BarEntry(4f, 2));
+        barEntriesArrayList.add(new BarEntry(5f, 4));
+        barEntriesArrayList.add(new BarEntry(6f, 1));
+
     }
 }
 
